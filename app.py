f559d97a01557428e248b6212383c92a6b7eef8d
Huy Ha <havanhuy1997@gmail.com>
2021-06-08 13:38:16 +0300

    Added user support

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import json
+import urllib
 
 import telegram
 import pymongo as mg
 from dotenv import load_dotenv
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, json
 
 import utils as ut
 
@@ -29,9 +29,18 @@ def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     if update["callback_query"]:
         callback_query_id = update["callback_query"]["id"]
-        print(callback_query_id)
+        send_from = update["callback_query"].to_dict()["from"]
+        user = {
+            "user_id": send_from["id"],
+            "user_name": send_from["username"],
+            "user_firstname": send_from["first_name"],
+            "user_lastname": send_from["last_name"],
+        }
         try:
-            bot.answer_callback_query(callback_query_id, url=url + 'game')
+            bot.answer_callback_query(
+                callback_query_id,
+                url=url + 'game?' + urllib.parse.urlencode(user)
+            )
         except Exception as e:
             bot.answer_callback_query(callback_query_id, str(e))
     return "ok"
@@ -39,9 +48,27 @@ def respond():
 
 @app.route("/game", methods=["GET"])
 def game():
+    user_id = request.args.get("user_id")
+    user_name = request.args.get("user_name")
+    user_first_name = request.args.get("user_firstname")
+    user_last_name = request.args.get("user_lastname")
+    user = my_collection.find_one({"user_id": user_id})
+    if not user:
+        user = {
+            "user_id": user_id,
+            "user_name": user_name,
+            "user_firstname": user_first_name,
+            "user_lastname": user_last_name,
+        }
+    else:
+        del user["_id"]
     return render_template(
         "index.html",
-        data={"url": url, "stock_codes": ut.load_data(STOCK_CODES_FILE)}
+        data={
+            "url": url,
+            "stock_codes": ut.load_data(STOCK_CODES_FILE),
+            "user": user,
+        }
     )
 
 
@@ -57,18 +84,38 @@ def submit():
         response = flask.jsonify({})
         add_headers(response)
         return response
+    print(request.data)
     data = json.loads(request.data)
     print(data)
-    if my_collection.find({"name": data.get("name")}).count() > 0:
-        res = {
-            "result": False,
-            "message": "Name already exists",
-        }
+    saved = False
+    user = my_collection.find_one({"user_id": data["user_id"]})
+    if user:
+        existing_answer_names = [answer["name"] for answer in user["answers"]]
+        if data["answer"]["name"] not in existing_answer_names:
+            user["answers"].append(data["answer"])
+            my_collection.update_one(
+                {"user_id": data.get("user_id")},
+                {"$set": {"answers": user["answers"]}}
+            )
+            saved = True
     else:
-        my_collection.insert_one(data)
+        my_collection.insert_one({
+            "user_id": data["user_id"],
+            "user_name": data["user_name"],
+            "user_firstname": data["user_firstname"],
+            "user_lastname": data["user_lastname"],
+            "answers": [data["answer"]]
+        })
+        saved = True
+    if saved:
         res = {
             "result": True,
             "message":  "Success",
+        }
+    else:
+        res = {
+            "result": False,
+            "message": "Answer name already exists",
         }
     res = flask.jsonify(res)
     add_headers(res)
